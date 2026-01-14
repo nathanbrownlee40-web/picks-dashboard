7c361cdf6c2e237ac0c9af17daccef97b8c355ef
nathanbrownlee40-web <nathanbrownlee40@gmail.com>
2026-01-14 19:50:32 +0000

    Update picks.js

--- src/data/picks.js
+++ src/data/picks.js
@@ -1,1 +1,26 @@
-src/data/picks.js
+const picks = [
+  {
+    id: 1,
+    match: "NEC Nijmegen vs PEC Zwolle",
+    league: "Eredivisie",
+    date: "2026-01-24",
+    probability: 0.95
+  },
+  {
+    id: 2,
+    match: "PSV vs Excelsior",
+    league: "Eredivisie",
+    date: "2026-01-10",
+    probability: 0.92
+  },
+  {
+    id: 3,
+    match: "Inter vs Pisa",
+    league: "Serie A",
+    date: "2026-01-23",
+    probability: 0.89
+  }
+];
+
+export default picks;
+
